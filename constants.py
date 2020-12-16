34ae7fd06acd2cb2387aabc23d8521bfb6557312
Improvemint <nemetchek.m@gmail.com>
2020-12-16 16:19:27 -0600

    Addtional Enums to improve readability

--- constants.py
+++ constants.py
@@ -5,3 +5,17 @@ class Action(IntEnum):
     STAND = 1
     DOUBLE = 2
     SPLIT = 3
+
+class Outcome(IntEnum):
+    WIN = 0
+    PUSH = 1
+    LOSE = 2
+
+class Dealer_Outcome(IntEnum):
+    SUM_17 = 0
+    SUM_18 = 1
+    SUM_19 = 2
+    SUM_20 = 3
+    SUM_21 = 4
+    BJ = 5
+    BUST = 6
