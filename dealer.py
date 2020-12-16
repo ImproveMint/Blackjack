dc710a23a43f03109679aca7c1d90bc9466bf338
Improvemint <nemetchek.m@gmail.com>
2020-12-16 16:20:25 -0600

    General updates to work with other classes

--- dealer.py
+++ dealer.py
@@ -3,10 +3,24 @@ from card import Card, Rank, Suit
 from shoe import Shoe
 from copy import deepcopy
 import probability, random
+from constants import Dealer_Outcome
+import numpy
 
 class Dealer():
 
-    poss = 0
+    dealer_outcome_probs_infinite = numpy.array([   [0.13078889978591976, 0.13078889978591976, 0.13078889978591976, 0.13078889978591976, 0.05386582286284283, 0.3076923076923077, 0.11528627030116942],
+                                                    [0.13980913952773533, 0.13490735037469442, 0.12965543342500774, 0.12402645577124097, 0.11799348450595983, 0, 0.3536081363953539],
+                                                    [0.13503398781114, 0.13048232645474486, 0.12558053730170396, 0.12032862035201726, 0.11469964269825049, 0, 0.3738748853821393],
+                                                    [0.1304897358495983, 0.12593807449320316, 0.12138641313680804, 0.11648462398376716, 0.11123270703408046, 0, 0.3944684455025419],
+                                                    [0.12225128527055074, 0.12225128527055078, 0.11769962391415566, 0.11314796255776056, 0.10824617340471966, 0, 0.41640366958226205],
+                                                    [0.16543817650334652, 0.10626657887021022, 0.10626657887021022, 0.10171491751381516, 0.09716325615742008, 0, 0.42315049208499683],
+                                                    [0.3685661937942386, 0.137796963025008, 0.0786253653918717, 0.0786253653918717, 0.07407370403547664, 0, 0.262312408361533],
+                                                    [0.12856654444917, 0.3593357752184008, 0.12856654444917012, 0.06939494681603388, 0.06939494681603389, 0, 0.24474124225119132],
+                                                    [0.11999544148589202, 0.11999544148589202, 0.3507646722551228, 0.1199954414858921, 0.06082384385275591, 0, 0.22842515943444525],
+                                                    [0.11142433852261402, 0.11142433852261402, 0.11142433852261402, 0.3421935692918448, 0.03450126159953709, 0.07692307692307693, 0.21210907661769918],
+                                                    [0.11142433852261402, 0.11142433852261402, 0.11142433852261402, 0.3421935692918448, 0.03450126159953709, 0.07692307692307693, 0.21210907661769918],
+                                                    [0.11142433852261402, 0.11142433852261402, 0.11142433852261402, 0.3421935692918448, 0.03450126159953709, 0.07692307692307693, 0.21210907661769918],
+                                                    [0.11142433852261402, 0.11142433852261402, 0.11142433852261402, 0.3421935692918448, 0.03450126159953709, 0.07692307692307693, 0.21210907661769918]])
 
     # Dealer defaults to the strategy I need for my application
     def __init__(self, shoe, stand=17, hit_soft=False):
@@ -33,20 +47,21 @@ class Dealer():
         self.up_card = None
 
     def playout_hand(self):
-        #Not good but I just want to get it working
-        #not proper probability distribution
+        # Assumes infinite deck
 
-        while self.hand.value < self.stand_min:
+        while self.hand.sum < self.stand_min:
             card = Card(Rank(random.randint(Rank.Ace, Rank.King)), Suit.Spade)
             self.hand.add_card(card)
 
-        return self.hand.value
+        return self.hand.sum
 
-    def dealer_outcome_probs(self):
+    def outcome_probs(self):
+        return Dealer.dealer_outcome_probs_infinite[int(self.hand.cards[0].rank)]
+
+    def calculate_outcome_probs(self):
         '''
         Returns the probabilities of possible outcomes for the dealer
         '''
-
         # dealer needs to have a hand to perform request
         # also check if it needs to be performed again
         if self.hand is not None:
@@ -71,35 +86,37 @@ class Dealer():
         p[5] = P(hand_value == BJ) # 21 with only 2 cards
         p[6] = P(hand_value > 21) # busted
 
-        At the very minimum we know that 3 cards are taken from the shoe and we
-        also know the dealer's up card.
+        Assumes infinite shoe
         '''
-        value = hand.value
-        index = 1 # always 1 since once the dealer starts playing we have no control
+        value = hand.sum
+        index = 1 # always 1 since we knew the dealer's single up card prior
 
         # dealer reached stopping condition add the probability of getting this hand to list
         if value > self.stand_min or (value == self.stand_min and not self.hit_soft):
 
-            # checks that number of cards of each rank didn't exceed shoe's capacity
-            ranks_left = deepcopy(self.shoe.ranks_left)
-
-            for card in hand.cards:
-                ranks_left[card.rank] = ranks_left[card.rank] -1;
-
-            if(min(ranks_left) < 0):
-                return
-
             #Dealer has BJ
-            elif hand.blackjack:
-                self.dealer_outcome_probabilities[5] += probability.probability_of_hand(self.shoe, hand, index)
+            if hand.blackjack:
+                self.dealer_outcome_probabilities[Dealer_Outcome.BJ] += probability.prob_cards(hand, index)
 
             #Dealer busted
             elif value > 21:
-                self.dealer_outcome_probabilities[6] += probability.probability_of_hand(self.shoe, hand, index)
+                self.dealer_outcome_probabilities[Dealer_Outcome.BUST] += probability.prob_cards(hand, index)
 
-            #Dealer has some value between 17 and 21 but not BJ
-            else:
-                self.dealer_outcome_probabilities[value-17] += probability.probability_of_hand(self.shoe, hand, index)
+            #Dealer has 21 but not BJ
+            elif value == 21:
+                self.dealer_outcome_probabilities[Dealer_Outcome.SUM_21] += probability.prob_cards(hand, index)
+
+            elif value == 20:
+                self.dealer_outcome_probabilities[Dealer_Outcome.SUM_20] += probability.prob_cards(hand, index)
+
+            elif value == 19:
+                self.dealer_outcome_probabilities[Dealer_Outcome.SUM_19] += probability.prob_cards(hand, index)
+
+            elif value == 18:
+                self.dealer_outcome_probabilities[Dealer_Outcome.SUM_18] += probability.prob_cards(hand, index)
+
+            elif value == 17:
+                self.dealer_outcome_probabilities[Dealer_Outcome.SUM_17] += probability.prob_cards(hand, index)
 
             return
 
